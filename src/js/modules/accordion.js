3e97d7e7bb129109297ce54433f7626cab088597
BogdanovBogdan <bogdanov_bogdanv@mail.ru>
2020-05-08 09:07:25 +0300

    Solution option 2 (also js + css)

--- src/js/modules/accordion.js
+++ src/js/modules/accordion.js
@@ -1,20 +1,25 @@
 const accordion = () => {
-	const tabs = document.querySelectorAll(".accordion-heading"),
-			tabContents = document.querySelectorAll(".accordion-block");
-
-	tabContents.forEach(item => {
-		item.classList.add("animated", "fadeInDown");
-	});
+	const tabs = document.querySelectorAll(".accordion-heading");
 
 	tabs.forEach(tab => {
-		tab.addEventListener("click", () => {
+		tab.addEventListener("click", function() {
 			tabs.forEach(item => {
 				item.classList.remove("ui-accordion-header-active");
+				item.nextElementSibling.style.maxHeight = "0px";
+				item.nextElementSibling.classList.remove("ui-accordion-content-active");
 			});
-
-			tab.classList.add("ui-accordion-header-active");
+			
+			this.classList.toggle("ui-accordion-header-active");
+			this.nextElementSibling.classList.add("ui-accordion-content-active");
+			
+			if (this.classList.contains("ui-accordion-header-active")) {
+				this.nextElementSibling.style.maxHeight = this.nextElementSibling.scrollHeight + 80 + "px";
+			} else {
+				this.nextElementSibling.style.maxHeight = "0px";
+				this.nextElementSibling.classList.remove("ui-accordion-content-active");
+			}
 		});
-	})
+	});
 };
 
 export default accordion;
