278833133545ad0b97acb17b750da2b4d5bee093
BogdanovBogdan <bogdanov_bogdanv@mail.ru>
2020-05-19 08:17:16 +0300

    accordion behavior fixed.

--- src/js/modules/accordion.js
+++ src/js/modules/accordion.js
@@ -4,16 +4,18 @@ const accordion = () => {
 	tabs.forEach(tab => {
 		tab.addEventListener("click", function() {
 			tabs.forEach(item => {
-				item.classList.remove("ui-accordion-header-active");
-				item.nextElementSibling.style.maxHeight = "0px";
-				item.nextElementSibling.classList.remove("ui-accordion-content-active");
+				if (this !== item) {
+					item.classList.remove("ui-accordion-header-active");
+					item.nextElementSibling.style.maxHeight = "0px";
+					item.nextElementSibling.classList.remove("ui-accordion-content-active");
+				}
 			});
-			
+
 			this.classList.toggle("ui-accordion-header-active");
-			this.nextElementSibling.classList.add("ui-accordion-content-active");
 			
 			if (this.classList.contains("ui-accordion-header-active")) {
 				this.nextElementSibling.style.maxHeight = this.nextElementSibling.scrollHeight + 80 + "px";
+				this.nextElementSibling.classList.add("ui-accordion-content-active");
 			} else {
 				this.nextElementSibling.style.maxHeight = "0px";
 				this.nextElementSibling.classList.remove("ui-accordion-content-active");
